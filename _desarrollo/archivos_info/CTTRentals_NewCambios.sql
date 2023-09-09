edcfc05eefca94a5358b9225d35d3dee4b48aa31
EdnaKL33 <ednayuritzy@gmail.com>
2023-09-08 18:57:25 -0600

    commit_edna

--- _desarrollo/archivos_info/CTTRentals_NewCambios.sql
+++ _desarrollo/archivos_info/CTTRentals_NewCambios.sql
@@ -1,3 +1,4 @@
+-- Modificaciones en la tabla de subletting
 ALTER TABLE `ctt_subletting`
 	ADD COLUMN `prd_id` INT(11) NULL DEFAULT NULL COMMENT 'ID del producto relacion con ctt_products' AFTER `cin_id`;
 
